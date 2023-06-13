149bd92a7a41799f46189212c6382805a43daa8f
Priya <128438275+theguardians0987@users.noreply.github.com>
2023-06-13 15:15:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ load_dotenv()
 
 Bot = Client(
     "QR Code Bot",
-    bot_token=os.environ.get("BOT_TOKEN"),
-    api_id=int(os.environ.get("API_ID")),
-    api_hash=os.environ.get("API_HASH"),
+    bot_token=os.environ.get("BOT_TOKEN", "5945201344:AAGuw0vSH5z3Lmc3S08bAtJEcKkZ2nAuftI"),
+    api_id=int(os.environ.get("API_ID", "29390803")),
+    api_hash=os.environ.get("API_HASH", "4227e730acf4483f59ad2799c2bd063f"),
     plugins=dict(root="plugins")
 )
 
